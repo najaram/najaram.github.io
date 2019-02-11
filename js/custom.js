47dac5217295d4f1732860815bc95876558b08e4
Jan <arambulo.jan@gmail.com>
2019-02-12 02:29:50 +0800

    add exp

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed('.banner__one h1', {
-  strings: ["Hi!, I'm Jan ^1000", "A self motivated ^1000", "Driven to succeed ^1000", "Autodidact"],
-  typeSpeed: 80,
-  backSpeed: 90,
-  loop: true
-});
